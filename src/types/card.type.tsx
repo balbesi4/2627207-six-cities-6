945292f872dea6e895868eb75eb73ec95e679375
balbesi4 <arseny.elkin@urfu.me>
2025-11-07 00:01:29 +0500

    second task done

---
+++ src/types/card.type.tsx
@@ -0,0 +1,9 @@
+export type Card = {
+  imageLink : string;
+  isPremium : boolean;
+  cost : number;
+  isInBookmarks : boolean;
+  rating : number;
+  description : string;
+  housingType : string;
+}
