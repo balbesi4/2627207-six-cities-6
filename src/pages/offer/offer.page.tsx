8ce4aaaa84489a7c764365159cdfc427136731fc
a.s.elkin <a.s.elkin@tbank.ru>
2025-12-08 23:47:44 +0500

    one more file

--- src/pages/offer/offer.page.tsx
+++ src/pages/offer/offer.page.tsx
@@ -96,7 +96,7 @@ export default function Offer(): JSX.Element {
               <div className="offer__host">
                 <h2 className="offer__host-title">Meet the host</h2>
                 <div className="offer__host-user user">
-                  <div className={`offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper`}>
+                  <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
                     <img className="offer__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
                   </div>
                   <span className="offer__user-name">Angelina</span>
